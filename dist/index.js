f3b4ec61e9bd494d86b495c2614a9e10073f075c
Andreas Bagge <andreasbaggenielsen@gmail.com>
2023-02-12 21:42:59 +0100

    fixes

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,18 @@
-import fs from 'fs';
-fs;
+// Require the necessary discord.js classes
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import * as dotenv from 'dotenv';
+const result = dotenv.config();
+if (result.error) {
+    throw result.error;
+}
+const token = process.env.TOKEN;
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+// Log in to Discord with your client's token
+client.login(token);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
-import fs from 'fs';
+// Require the necessary discord.js classes
+import { Client, Events, GatewayIntentBits } from "discord.js";
 
-fs;
+import * as dotenv from 'dotenv';
+
+const result = dotenv.config();
+
+  if (result.error) {
+    throw result.error;
+  }
+
+
+const token: string = process.env.TOKEN;
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
