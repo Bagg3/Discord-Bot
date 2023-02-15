9b479c90c277c3f1331e17c620cdf1621fe6c749
Andreas Bagge <andreasbaggenielsen@gmail.com>
2023-02-15 22:31:51 +0100

    LORT FUNGERER IKKE

--- dist/index.js
+++ dist/index.js
@@ -7,29 +7,21 @@ if (result.error) {
 }
 const token = process.env.TOKEN;
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent,] });
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
-// Log in to Discord with your client's token
 client.login(token);
-/*
-client.on('message',
-function (messages){
-    if(messages.content.toLocaleLowerCase()==='bagge')
-    messages.channel.send('Lizards'); //reply hello word message with senders name
-})
-*/
-client.on("message", msg => {
-    if (msg.content === "bagge") {
-        msg.reply("Lizards");
-    }
+//register message event listener and reply to message
+client.on('messageCreate', function (messageCreate) {
+    if (messageCreate.content.toLocaleLowerCase() === 'bagge')
+        messageCreate.channel.send('Lizards'); //reply hello word message with senders name
 });
-client.on("message", msg => {
-    if (msg.content === "Bagge") {
-        msg.reply("Lizards");
-    }
+client.on("messageCreate", async (message) => {
+    console.log('messageCreate');
+    if (message.author.bot)
+        return;
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -9,39 +9,27 @@ const result = dotenv.config();
     throw result.error;
   }
 
-
 const token: string = process.env.TOKEN;
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent,] });
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-// Log in to Discord with your client's token
 client.login(token);
 
-/*
-client.on('message',
-function (messages){
-    if(messages.content.toLocaleLowerCase()==='bagge') 
-    messages.channel.send('Lizards'); //reply hello word message with senders name
+//register message event listener and reply to message
+client.on('messageCreate', 
+function (messageCreate){
+    if(messageCreate.content.toLocaleLowerCase()==='bagge') 
+    messageCreate.channel.send('Lizards'); //reply hello word message with senders name
+});
+
+client.on("messageCreate", async (message) => {
+  console.log('messageCreate');
+  if (message.author.bot) return;
 })
-*/
-
-client.on("message", msg => {
-  if (msg.content === "bagge") {
-    msg.reply("Lizards");
-  }
-})
-
-
-client.on("message", msg => {
-  if (msg.content === "Bagge") {
-    msg.reply("Lizards");
-  }
-})
-
