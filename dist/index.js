e6e757a3103982003e217826983807ead81d2378
Andreas Bagge <andreasbaggenielsen@gmail.com>
2023-02-15 23:03:55 +0100

    Works now

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 // Require the necessary discord.js classes
 import { Client, Events, GatewayIntentBits } from "discord.js";
+import { EmbedBuilder } from 'discord.js';
 import * as dotenv from 'dotenv';
 const result = dotenv.config();
 if (result.error) {
@@ -7,21 +8,38 @@ if (result.error) {
 }
 const token = process.env.TOKEN;
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent,] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 client.login(token);
-//register message event listener and reply to message
+// Bonk gif
+const exampleEmbed = new EmbedBuilder()
+    .setImage('https://c.tenor.com/yHX61qy92nkAAAAC/yoshi-mario.gif');
+//Send bonk gif when bagge is written
 client.on('messageCreate', function (messageCreate) {
     if (messageCreate.content.toLocaleLowerCase() === 'bagge')
-        messageCreate.channel.send('Lizards'); //reply hello word message with senders name
+        messageCreate.channel.send({ embeds: [exampleEmbed] });
 });
+client.on('messageCreate', function (messageCreate) {
+    if (messageCreate.content.toLocaleLowerCase() === 'wait')
+        messageCreate.channel.send('Waiting for 5 seconds');
+    //setTimeout(5000);
+    messageCreate.channel.send('5 seconds have passed');
+    //reply hello word message with senders name
+});
+// wait for 5 seconds
+//setTimeout(5000);
+// Link to bonk gif
+//https://c.tenor.com/yHX61qy92nkAAAAC/yoshi-mario.gif
+//Error handling
+/*
 client.on("messageCreate", async (message) => {
-    console.log('messageCreate');
-    if (message.author.bot)
-        return;
-});
+  console.log('messageCreate');
+  if (message.author.bot) return;
+})
+
+*/ 
 //# sourceMappingURL=index.js.map
